457be7d086814884fdcad550dee22687e0466e45
wallacevilela7 <wallace.vilela9@gmail.com>
2026-01-27 15:30:10 -0300

    Create spring boot app

---
+++ src/test/java/wvs/cryptography/CryptographyApplicationTests.java
@@ -0,0 +1,13 @@
+package wvs.cryptography;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptographyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
